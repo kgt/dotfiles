38d996b193502da84759a7b26ead8edad87f9fd1
kgt <kagetsu.34@gmail.com>
2014-12-15 20:46:28 +0900

    Enable show-paren-mode on ruby-mode

--- .emacs.d/inits/50-ruby-mode.el
+++ .emacs.d/inits/50-ruby-mode.el
@@ -1,4 +1,6 @@
 (use-package ruby-mode
   :mode (("/Rakefile\\'"  . ruby-mode)
 	 ("/Gemfile\\'"   . ruby-mode)
-	 ("\\.gemspec\\'" . ruby-mode)))
+	 ("\\.gemspec\\'" . ruby-mode))
+  :config
+  (add-hook 'ruby-mode-hook 'show-paren-mode))
