fe3c04af3e1639777c40a622536c5c6a6f473985
kgt <kagetsu.34@gmail.com>
2014-12-16 03:29:22 +0900

    Rename

--- .emacs.d/inits/50-sh-script.el
+++ .emacs.d/inits/50-sh-script.el
@@ -1,7 +1,7 @@
 (use-package sh-script
   :defer t
   :config
-  (defun sh-mode-hook-indent ()
+  (defun set-sh-mode-indent ()
     (setq sh-basic-offset 2)
     (setq sh-indentation 2))
-  (add-hook 'sh-mode-hook 'sh-mode-hook-indent))
+  (add-hook 'sh-mode-hook 'set-sh-mode-indent))
