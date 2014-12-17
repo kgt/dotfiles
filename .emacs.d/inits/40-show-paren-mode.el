9bd35b20a1c105b339f21f0d1d6dc08a31f652f6
kgt <kagetsu.34@gmail.com>
2014-12-17 19:24:41 +0900

    Clean configs for show-paren-mode

--- .emacs.d/inits/40-show-paren-mode.el
+++ .emacs.d/inits/40-show-paren-mode.el
@@ -1,4 +1,1 @@
-(defun make-show-paren-mode-buffer-local (&rest r)
-  (make-variable-buffer-local 'show-paren-mode))
-
-(advice-add 'show-paren-mode :before 'make-show-paren-mode-buffer-local)
+(make-variable-buffer-local 'show-paren-mode)
