c6708436bfd1e80d1e38a6c1d7ed6743a7be8c4c
kgt <kagetsu.34@gmail.com>
2017-03-15 01:48:51 +0900

    Use color-theme-solarized instead of solarized-theme
    solarized-theme don't work well in terminal

---
+++ .emacs.d/inits/50-color-theme-solarized.el
@@ -0,0 +1,5 @@
+(cond ((member 'solarized (custom-available-themes))
+       (let ((mode 'dark))
+	 (set-frame-parameter nil 'background-mode mode)
+	 (set-terminal-parameter nil 'background-mode mode))
+       (load-theme 'solarized t)))
