f7714d7675974bcd6e4912e7c68425da9fb24394
kgt <kagetsu.34@gmail.com>
2017-03-24 02:15:06 +0900

    Remove unnecessary hook

--- .emacs.d/inits/50-sh-script.el
+++ .emacs.d/inits/50-sh-script.el
@@ -1,7 +1,5 @@
 (use-package sh-script
   :defer t
   :config
-  (defun set-sh-mode-indent ()
-    (setq sh-basic-offset 2)
-    (setq sh-indentation 2))
-  (add-hook 'sh-mode-hook 'set-sh-mode-indent))
+  (setq sh-basic-offset 2)
+  (setq sh-indentation 2))
